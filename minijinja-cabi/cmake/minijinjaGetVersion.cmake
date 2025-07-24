ff639249a0e502aa1efff3ca4e326df6516ef3dd
khj809 <onsealeatang@gmail.com>
2025-07-24 21:10:23 +0900

    fix

--- minijinja-cabi/cmake/minijinjaGetVersion.cmake
+++ minijinja-cabi/cmake/minijinjaGetVersion.cmake
@@ -1,17 +1,20 @@
 # Get minijinja version from Cargo.toml
 
-# 1. Read the contents of Cargo.toml into a variable
-file(READ "${CMAKE_CURRENT_SOURCE_DIR}/Cargo.toml" CARGO_TOML_CONTENTS)
+# 1. Get the absolute path to Cargo.toml, which is one directory up.
+#    CMAKE_CURRENT_LIST_DIR is the key variable here.
+get_filename_component(CARGO_TOML_PATH "${CMAKE_CURRENT_LIST_DIR}/../Cargo.toml" ABSOLUTE)
 
-# 2. Use a regular expression to find the version line and capture the version number
+# 2. Read the contents of the file
+file(READ "${CARGO_TOML_PATH}" CARGO_TOML_CONTENTS)
+
+# 3. Use a regular expression to find the version line and capture the version number
 # This looks for 'version = "x.y.z"' and saves "x.y.z"
 string(REGEX MATCH "version[ \t]*=[ \t]*\"([0-9\.]+)\""
     CARGO_VERSION_MATCH "${CARGO_TOML_CONTENTS}")
 
-# 3. The captured version is in the special variable CMAKE_MATCH_1.
+# 4. The captured version is in the special variable CMAKE_MATCH_1.
 # We assign it to our own variable for clarity and error check it.
 set(MINIJINJA_VERSION "${CMAKE_MATCH_1}")
-
 if(NOT MINIJINJA_VERSION)
     message(FATAL_ERROR "❌ Could not parse minijinja version from Cargo.toml")
 endif()
